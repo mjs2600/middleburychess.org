56ee3762e8fda4940132e968e8aecd7ec5a029a0
Michael Simpson <mjs2600@gmail.com>
2021-07-14 17:04:28 -0400

    Update site

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,6 @@ function IndexPage() {
         <section className="text-left">
           <h3>Upcoming Meeting Times</h3>
           <ul>
-            <li>Wednesday, July 14, 1:30 AM to 4:00 PM at <a href="https://g.page/11th-Hour-Botanicals?share">11th Hour Botanicals</a>.</li>
             <li>Saturday, July 17, 9:30 AM to 12:30 PM at <a href="https://g.page/11th-Hour-Botanicals?share">11th Hour Botanicals</a>.</li>
           </ul>
         </section>
