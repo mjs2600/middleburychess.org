cabc9ab197d64b627878937661e0a31c4903317e
Michael Simpson <mjs2600@gmail.com>
2021-10-28 20:30:24 -0400

    Add explicit schema and update dates

---
+++ gatsby-node.js
@@ -0,0 +1,18 @@
+exports.createSchemaCustomization = ({actions, schema}) => {
+  const {createTypes} = actions
+
+  // Type definitions can be provided in SDL
+  const typeDefs = `
+  type LocationsJson implements Node {
+    directions: String!
+    name: String!
+  }
+  type MeetingsJson implements Node {
+    location: LocationsJson @link(by: "slug")
+    startTime: Date
+    endTime: Date
+  }
+  `
+  createTypes(typeDefs)
+}
+
