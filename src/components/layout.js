97ac7d4c85941df887ea5cc68273bf45ddfe391d
Michael Simpson <mjs2600@gmail.com>
2022-02-09 09:12:15 -0500

    Add missing line

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import Header from "./header";
 // import '@fortawesome/fontawesome-svg-core/styles.css';
 // Prevent fontawesome from adding its CSS since we did it manually above:
 // import {config} from '@fortawesome/fontawesome-svg-core';
-config.autoAddCss = false; /* eslint-disable import/first */
+// config.autoAddCss = false; /* eslint-disable import/first */
 
 function Layout({children}) {
 
